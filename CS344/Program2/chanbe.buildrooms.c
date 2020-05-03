84aeb9786c55c408143872b436e7255b168f4467
Alyzzar <zhanben00@gmail.com>
2020-05-02 23:44:05 -0700

    Update chanbe.buildrooms.c

--- CS344/Program2/chanbe.buildrooms.c
+++ CS344/Program2/chanbe.buildrooms.c
@@ -233,7 +233,7 @@ void exportRooms(){
 	int i;
 	int j;
 	int pid = getpid();
-	int buffer = 64 * sizeof(char);
+	int buffer = 128 * sizeof(char);
 	int file_descriptor;
 	struct stat st = {0};
 	char *dir_name = malloc(buffer);
