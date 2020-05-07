9a4b86f19cc77a23bb8fced14664267382c80f6d
Alyzzar <zhanben00@gmail.com>
2020-05-06 22:34:05 -0700

    Update chanbe.adventure.c

--- CS344/Program2/chanbe.adventure.c
+++ CS344/Program2/chanbe.adventure.c
@@ -214,9 +214,7 @@ void parseRoom(FILE* f, struct game* game){
 	size_t line_size = 32;
 	char** lines = (char**)malloc(sizeof(char*) * 32);
 	
-	for(i = 0; i < 10; i++){
-		lines[i] = {NULL};
-	}
+	lines[] = {NULL};
 	
 	// Reset file pointer, and loop to store file as array
 	fseek(f, 0, SEEK_SET);
