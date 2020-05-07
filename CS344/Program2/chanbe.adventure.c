dc2b97b2f9718f4fd8b46b3e2d2351c565dec656
Alyzzar <zhanben00@gmail.com>
2020-05-06 18:14:30 -0700

    Update chanbe.adventure.c

--- CS344/Program2/chanbe.adventure.c
+++ CS344/Program2/chanbe.adventure.c
@@ -284,9 +284,9 @@ int findType (struct game* game, const char* type){
 			printf("Successful.\n");
 			printf(" - - - Getting lines...\n");
 			j = 1;
-			do{ numChars = getline(&line, &buffer, f);
-			} while (numChars != -1) {
-				line[numChars - 1] = '\0';
+			
+			while (getline(&line, &buffer, f) != 1) {
+				line[strlen(line) - 1] = '\0';
 				printf(" - - - - LINE %d: %s\n", j, line);
 				//Look for line with substring matching type
 				if (strstr(line, type) != NULL) {
