57b92288d0e1490341d0ee96ed34680d10537153
Alyzzar <zhanben00@gmail.com>
2020-07-27 17:31:46 -0700

    Initial commit for OTP

---
+++ CS344_Summer/chanbe.otp/keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "otp.h"
+
+void genKey(){
+	int n, r;
+	char key[len + 1]
+	
+	for (n = 0; n < len; n++){
+		//Rand int from 0 to 27
+		n = rand() % 27;
+		//Use rand into to pull from array in otp.h
+		key[n] = alph[r];
+	}
+	//Good practice to null terminate generated strings
+	key[len] = '\0'
+	printf("%s\n", key);
+}
+
+int main (int argc, char* argv[]){
+	srand(time(0));
+	//Generate random key with the given length
+	genKey(atoi(argv[1]));
+	return 0;
+}
