e9fe0bf69016a3b92df343832495b6449a769284
Alyzzar <zhanben00@gmail.com>
2020-07-27 18:30:52 -0700

    Update otp.c

--- CS344_Summer/chanbe.otp/otp.c
+++ CS344_Summer/chanbe.otp/otp.c
@@ -125,13 +125,19 @@ otp_c (char* f_plaintext, char* f_key, char* port_str, char option) {
 //port #		option
 otp_d (char* port_str, char option) {
 	//OTP for server side
-	int len, sock_fd, c_read;
+	int len, sock_fd, conn_fd, c_read;
+	int pid = getpid();
 	int port = atoi(port_str);
+	
+	struct sockaddr_in serverAddress, clientAddress;
 	
 	char buffer[512];
 	char output[80000];
+	char f_output[256];
 	char plaintext[80000];
+	char f_plaintext[256];
 	char key[80000];
+	char f_key[256];
 	
 	//Create and allocate a server struct (same as above)
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress));
@@ -141,8 +147,93 @@ otp_d (char* port_str, char option) {
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
 	
 	// Set up the socket
-	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	if (listenSocketFD < 0) error("ERROR opening socket");
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (sock_fd < 0) error("ERROR: Could not open socket.\n");
+	if (bind(sock_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
+		error ("ERROR: Could not bind socket.\n");
+	}
+	//Turn on the socket with max connections = 5
+	listen(sock_fd, MAX_CONNECTIONS);
 	
+	while(1){
+		// Get address size for client
+		clientSize = sizeof(clientAddress);
+		// Accept connection
+		conn_fd = accept(sock_fd, (struct sockaddr *)&clientAddress, &clientSize);
+		if (conn_fd < 0) error("ERROR: Could not accept connection.\n");
+		
+		// Create child process
+		pid = fork();
+		switch (pid) {
+			case -1:	;
+				//Error. Terminate program
+				error ("ERROR: Could not create child process.\n");
+				exit(0);
+				break;
+			case 0:		;
+				//Process created successfully
+				memset(buffer, '\0', sizeof(buffer));
+				c_read = recv(conn_fd, buffer, (sizeof(buffer) - 1), 0);
+				if (c_read < 0) {
+					error ("ERROR: Could not read from socket.\n");
+				}
+				
+				// Array to find /0 and /n delimiters via strtok
+				const char newline[2] = {'\n', '\0'};
+				
+				//Grab plaintext from top of buffer
+				char *token = strtok(buffer, newline);
+				strcpy(f_plaintext, token);
+				//Grab key from buffer (NULL --> pointer continues from previous call)
+				token = strtok(NULL, newline);
+				strcpy(f_key, token);
+
+				// Make sure the right program is connecting
+				token = strtok(NULL, newline);
+				if (strcmp(option, token)) {
+					fprintf(stderr, "ERROR %s cannot use %s_d.\n", token, option );
+					wrongFile = 1;
+				}
+				
+				//Generate file, export contents of output
+				sprintf(f_output, "%s_f.%d", option, pid);
+				FILE* fd_output = fopen(f_output, "w+");
+				
+				//Correct file
+				if (wrongfile == 0){
+					parseFile(f_plaintext, &plaintext);
+					parseFile(f_key, &key);
+					
+					if (option == 'e'){
+						enc(plaintext, output, key, strlen(plaintext));
+					} else if (option == 'd'){
+						dec(plaintext, output, key, strlen(plaintext));
+					}
+					//Correct ouput. Print the en/decoded data into the file
+					fprintf(fd_output, "%s", output);
+				} else {
+					//Incorrect output. Save and close the empty file.
+					fprintf(fd_output, "");
+				}
+				fclose(fd_output);
+				
+				//Send the file back to the client, close the connection
+				c_read = send(conn_fd, f_output, strlen(f_output), 0);
+				if (c_read < 0) error ("ERROR: Could not write to socket.\n");
+				close(conn_fd);
+				conn_fd = -1;
+				exit(0);
+				
+				break;
+			default:		;
+		}
+		//End specific connections, kill processes
+		close(conn_fd);
+		conn_fd = -1;
+		wait(NULL);
+	}
 	
+	//Close connection to the server completely
+	close (sock_fd);
+	return 0;
 }
