7910c13c8a8034fd7cb2cde455281c018add9e3f
Alyzzar <zhanben00@gmail.com>
2020-07-27 18:30:04 -0700

    Update otp.h

--- CS344_Summer/chanbe.otp/otp.h
+++ CS344_Summer/chanbe.otp/otp.h
@@ -1,19 +1,15 @@
 #ifndef OTP_H
 #define OTP_H
 
+#define MAX_CONNECTIONS 5
+
 const char alph[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
 void enc(char, char, char, int);
 void dec(char, char, char, int);
 
-//OTP_c args
-//argv[1]	argv[2]		argv[3] 	'e'||'d'
-//text		key 		port #		option
 int otp_c(char*, char*, char*, char);
 
-//OTP_c args
-//argv[1]		'e'||'d'
-//port #		option
 int otp_d(char*, char);
 
 #endif
