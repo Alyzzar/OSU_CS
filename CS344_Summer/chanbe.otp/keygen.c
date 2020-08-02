7a898baf16f9189e2075ee5d27faee6b680ba34d
Alyzzar <zhanben00@gmail.com>
2020-08-02 14:05:12 -0700

    Update keygen.c

--- CS344_Summer/chanbe.otp/keygen.c
+++ CS344_Summer/chanbe.otp/keygen.c
@@ -5,9 +5,9 @@
 
 #include "otp.h"
 
-void genKey(){
+void genKey(int len){
 	int n, r;
-	char key[len + 1]
+	char key[len + 1];
 	
 	for (n = 0; n < len; n++){
 		//Rand int from 0 to 27
@@ -16,7 +16,7 @@ void genKey(){
 		key[n] = alph[r];
 	}
 	//Good practice to null terminate generated strings
-	key[len] = '\0'
+	key[len] = '\0';
 	printf("%s\n", key);
 }
 
