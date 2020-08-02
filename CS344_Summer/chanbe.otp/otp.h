fc18ac06f61b03d59e466cbecb90043877d13422
Alyzzar <zhanben00@gmail.com>
2020-08-02 14:17:34 -0700

    Update otp.h

--- CS344_Summer/chanbe.otp/otp.h
+++ CS344_Summer/chanbe.otp/otp.h
@@ -5,11 +5,19 @@
 
 const char alph[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
-void enc(char, char, char, int);
-void dec(char, char, char, int);
+void intToStr (int, char, int)
+void strToInt (char, int, int)
+void enc (char, char, char, int);
+void dec (char, char, char, int);
 
-int otp_c(char*, char*, char*, char);
+//OTP_c args
+//argv[1]	argv[2]		argv[3] 	'e'||'d'
+//text		key 		port #		option
+int otp_c (char*, char*, char*, char);
 
+//OTP_c args
+//argv[1]		'e'||'d'
+//port #		option
 int otp_d(char*, char);
 
 #endif
