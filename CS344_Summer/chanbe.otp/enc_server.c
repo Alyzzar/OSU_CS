6cda5842be26880788a0f5877f88503d4a5b683b
Alyzzar <zhanben00@gmail.com>
2020-08-03 17:23:19 -0700

    Update enc_server.c

--- CS344_Summer/chanbe.otp/enc_server.c
+++ CS344_Summer/chanbe.otp/enc_server.c
@@ -1,13 +1,13 @@
+/**		Assignment 3: One-Time Pads  
+  *		Written by Ben Chan for OSU CS 344, Summer 2020  
+  *		
+  *		This is the server-side encoder. This calls otp_s from otp.
+  **/
+  
 #include "otp.h"
 
 int main (int argc, char *argv[]){
-	
-	if (argc < 2) {
-		//Not enough arguments
-		exit(0);
-	}
-	
+	if (argc < 2) exit(0);	//Not enough arguments
 	otp_s(argv[1], "e");
-	
 	return 0;
 }
