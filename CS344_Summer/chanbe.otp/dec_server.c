ce75b875e608c4767c6495a110b609412975ac88
Alyzzar <zhanben00@gmail.com>
2020-08-03 17:43:39 -0700

    Update dec_server.c

--- CS344_Summer/chanbe.otp/dec_server.c
+++ CS344_Summer/chanbe.otp/dec_server.c
@@ -1,13 +1,13 @@
 /**		Assignment 3: One-Time Pads  
   *		Written by Ben Chan for OSU CS 344, Summer 2020  
   *		
-  *		This is the client-side encoder. This calls otp_c from otp.
+  *		This is the server-side decoder. This calls otp_s from otp.
   **/
-
+  
 #include "otp.h"
 
 int main (int argc, char *argv[]){
-	if (argc < 4) exit(0);	//Not enough arguments
-	otp_c(argv[1], argv[2], argv[3], "enc");
+	if (argc < 2) exit(0);	//Not enough arguments
+	otp_s(argv[1], "dec");
 	return 0;
 }
