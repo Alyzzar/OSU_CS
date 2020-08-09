a7e3fc6cd00f24862ef50f6d8915986333cb7dbf
Alyzzar <zhanben00@gmail.com>
2020-08-08 21:53:17 -0700

    Missing ()

--- CS344_Summer/chanbe.multithread/line_processor.c
+++ CS344_Summer/chanbe.multithread/line_processor.c
@@ -160,7 +160,7 @@ int sign_parse(){
 				&& buffer[(i + 1) % SIZE] == '+'){
 			if(DEBUG) printf("	(SIGN_PARSE) - Matching '++' found.\n");
 			//Matching pair found
-			buffer[(i % SIZE] = '^';
+			buffer[i % SIZE] = '^';
 			//Set this value to '*' for clarity purposes (Should be overwritten anyways)
 			buffer[(i + 1) % SIZE] = '*';
 			//Perform an index shift, from curr index to end of buffer
