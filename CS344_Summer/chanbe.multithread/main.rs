c5c849a4babb30ade42538671ea9269e3a34320b
Alyzzar <zhanben00@gmail.com>
2020-08-11 17:26:04 -0700

    Update main.rs

--- CS344_Summer/chanbe.multithread/main.rs
+++ CS344_Summer/chanbe.multithread/main.rs
@@ -119,7 +119,7 @@ fn main() {
 		let counter = Arc::new(Mutex::new(0));
 		let mut handles = vec![];
 		
-		for _ in 0..10 {
+		for i in 0..10 {
 			let counter = Arc::clone(&counter);
 			let handle = thread::spawn(move || {
 				let mut num = counter.lock().unwrap();
