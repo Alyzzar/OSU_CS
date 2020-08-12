305b4eeafc06c7eb7dc31536f1c7fb737d09664c
Alyzzar <zhanben00@gmail.com>
2020-08-12 13:46:07 -0700

    Update final.c

--- CS344_Summer/final.c
+++ CS344_Summer/final.c
@@ -5,8 +5,23 @@
 #include <unistd.h>
 
 int main (int argc, char* argv[]){
-	char* str1 = "Foo";
-char* str2 = "FooBar";
-int n = strlen(str1) < strlen(str2) ? strlen(str1) : strlen(str2);
-printf("%d, %d\n", n, strncmp(str1, str2, n));
+	int r, pipeFDs[2];
+	char message[512];
+	pid_t spawnpid;
+
+	pipe(pipeFDs);
+	spawnpid = fork();
+
+	switch (spawnpid){
+	case 0:
+	  close(pipeFDs[0]); // close the input file descriptor
+	  write(pipeFDs[1], "hi process, this is the STUFF!!", 21);
+	  break;
+
+	default:
+	  close(pipeFDs[1]); // close output file descriptor
+	  r = read(pipeFDs[0], message, sizeof(message));
+	  printf("Message received from other: %s\n", message);
+	  break;
+	}
 }
